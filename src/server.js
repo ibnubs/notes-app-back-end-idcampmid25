bbfbfda187ba01370ea0c17df31f1d0c6d8db727
ibnubswork <ibnubs.work@gmail.com>
2025-01-21 21:53:38 +0700

    postgres database implemented

--- src/server.js
+++ src/server.js
@@ -1,15 +1,28 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
-const NotesService = require('./services/inMemory/NotesServices');
+// const NotesService = require('./services/inMemory/NotesServices');
+const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 const ClientError = require('./exceptions/ClientError');
+const { required } = require('joi');
+const { configDotenv } = require('dotenv');
 
 const init = async () => {
   const notesService = new NotesService();
 
+  // const server = Hapi.server({
+  //   port: 5000,
+  //   host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+  //   routes: {
+  //     cors: {
+  //       origin: ['*'],
+  //     },
+  //   },
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
