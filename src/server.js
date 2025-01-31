e45f6e6ee000864d3dd3b8888098a74de0b2b220
ibnubswork <ibnubs.work@gmail.com>
2025-01-31 23:34:33 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,25 +1,25 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+
+// notes
 const notes = require('./api/notes');
-// const NotesService = require('./services/inMemory/NotesServices');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
+
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const ClientError = require('./exceptions/ClientError');
-const { required } = require('joi');
-const { configDotenv } = require('dotenv');
+// const { required } = require('joi');
+// const { configDotenv } = require('dotenv');
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
 
-  // const server = Hapi.server({
-  //   port: 5000,
-  //   host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-  //   routes: {
-  //     cors: {
-  //       origin: ['*'],
-  //     },
-  //   },
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST !== 'production' ? 'localhost' : '0.0.0.0',
@@ -30,13 +30,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
